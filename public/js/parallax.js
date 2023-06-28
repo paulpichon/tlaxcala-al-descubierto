0596402c7ca98c89e12569ef36621f31302c34a7
Paul Pichon F <paul10_barca@hotmail.com>
2023-06-27 20:15:35 -0600

    Se cambio el #scene en el JS por una clase parallaxContenedor

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,6 +1,7 @@
 // Funcion para iniciar parallax.js
 const parallax = () => {
-    const scene = document.getElementById('scene');
+    // const scene = document.getElementById('scene'); esto es lo que originalmente viene el el codigo parallax
+    const scene = document.querySelector('.parallaxContenedor');
     const parallaxInstance = new Parallax(scene);
 }
 // exports
