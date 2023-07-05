83c4029f4e0bbdad71bdaf9644ecf20727d54b61
paul pichon <paul10_barca@hotmail.com>
2023-07-04 20:39:41 -0600

    Se agrega parallax a segunda imagen pagina NOSOTROS

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -3,6 +3,8 @@ const parallax = () => {
     // const scene = document.getElementById('scene'); esto es lo que originalmente viene el el codigo parallax
     const scene = document.querySelector('.parallaxContenedor');
     const parallaxInstance = new Parallax(scene);
+    const scene2 = document.querySelector('.parallaxContenedor2');
+    const parallaxInstance2 = new Parallax(scene2);
 }
 // exports
 export {
