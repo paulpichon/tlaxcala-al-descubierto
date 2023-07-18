61ee199bf430c26ed4a87d1054ef3b80a5fc5e3f
paul pichon <paul10_barca@hotmail.com>
2023-07-17 22:29:20 -0600

    Update parallax.js se agrego condiciones para verificar si existen ciertas variables

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -2,9 +2,13 @@
 const parallax = () => {
     // const scene = document.getElementById('scene'); esto es lo que originalmente viene el el codigo parallax
     const scene = document.querySelector('.parallaxContenedor');
-    const parallaxInstance = new Parallax(scene);
+    if ( scene ) {
+        const parallaxInstance = new Parallax(scene);
+    }
     const scene2 = document.querySelector('.parallaxContenedor2');
-    const parallaxInstance2 = new Parallax(scene2);
+    if ( scene2 ) {
+        const parallaxInstance2 = new Parallax(scene2);
+    }
 }
 // exports
 export {
