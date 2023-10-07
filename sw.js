cb7f38d54fd22debe1cb60e25702cd544f678d5b
paul pichon <paul10_barca@hotmail.com>
2023-10-07 01:02:43 -0600

    Create: creacion de APP_SHELL y APP_SHELL_INMUTABLE

---
+++ sw.js
@@ -0,0 +1,24 @@
+
+//Cache
+const STATIC_CACHE    = 'static-v1';
+const DYNAMIC_CACHE   = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+//Todo lo necesario para la aplicacion
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'public/css/index.css',
+    'public/img/tlaxcallan.png',
+    'app.js'
+];
+
+//Todo lo necesario pero que no se va a modificar JAMAS
+const APP_SHELL_INMUTABLE = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    'https://fonts.googleapis.com',
+    'https://fonts.gstatic.com',
+    'https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto+Mono:ital@1&family=Roboto:ital,wght@1,300&display=swap',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js'
+];
