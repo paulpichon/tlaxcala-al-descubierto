9151622a176f333e2d0b9778ebec4c9b8342f9d9
paul pichon <paul10_barca@hotmail.com>
2023-10-10 22:28:42 -0600

    create: cache con network fallback

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+//imports
+importScripts('public/js/sw-utils.js');
 
 //Cache
 const STATIC_CACHE    = 'static-v1';
@@ -16,9 +18,66 @@ const APP_SHELL = [
 //Todo lo necesario pero que no se va a modificar JAMAS
 const APP_SHELL_INMUTABLE = [
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-    'https://fonts.googleapis.com',
-    'https://fonts.gstatic.com',
-    'https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto+Mono:ital@1&family=Roboto:ital,wght@1,300&display=swap',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js'
 ];
+
+//Instalacion
+self.addEventListener('install', e => {
+
+    //Almacenar en CACHE APP_SHELL y APP_SHELL_INMUTABLE
+    const cacheStatic = caches.open( STATIC_CACHE ).then( cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then( cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
+});
+
+//Proceso para borrar los CACHES anteriores que ya no van a servir
+self.addEventListener('activate', e => {
+    //verificar si el cache actual del SW es la misma del que se encuentra activo no se debe de hacer nada
+
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+            
+            //static
+            if ( key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete( key );
+            }
+
+        });
+
+    });
+
+    e.waitUntil( respuesta );
+
+});
+
+//cache con network fallback
+self.addEventListener('fetch', e => {
+
+    //verificar en el CACHE si existe LA REQUEST
+    const respuesta = caches.match( e.request ).then( res => {
+        
+        //si existe la respuesta devolvemos la respuesta
+        if ( res ) {
+            return res;
+        } else {
+            //si no existe la respuesta
+            
+            return fetch( e.request ).then( newRes => {
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+            });
+            
+        }
+    });
+
+
+    //respuesta
+    e.respondWith( respuesta );
+
+});
